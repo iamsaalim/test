b2a847b5aa24be8287728bfc310fdb89d26988b0
Saalim.hussain <Saalim.hussain@HMECL002394.happiestminds.com>
2021-12-27 11:06:18 +0530

    Sample

--- Demo/src/com/example/Main.java
+++ Demo/src/com/example/Main.java
@@ -1,11 +1,11 @@
 package com.example;
 
 public class Main {
-public static void main(String[] args) {
-	
-	
-	System.out.println("welcome to java");
-	System.out.println("welcome to java");
-	
-}
+	public static void main(String[] args) {
+
+		System.out.println("welcome to java");
+		System.out.println("welcome to Angular");
+		System.out.println("Welcome to git");
+
+	}
 }
